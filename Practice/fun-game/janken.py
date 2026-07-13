cc2e26a976ff8335cb443be85acefb632d03c01d
takkun2355 <92196121+takkun2355@users.noreply.github.com>
2026-07-14 07:57:17 +0900

    Commit files and janken.py

---
+++ Practice/fun-game/janken.py
@@ -0,0 +1,21 @@
+
+def yourwin():
+    print("a")
+
+def yourlose():
+    print()
+
+def yourdraw():
+    print()
+
+def random():
+    print()
+    
+def pon():
+    print()
+
+if __name__ == "__main__":
+    yourwin()
+    #yourlose()
+    #yourdraw()
+    #random()
