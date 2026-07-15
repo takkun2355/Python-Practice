a1d0a29fcddecb44a2ba880b38c7b28b3fc9a89f
takkun2355 <92196121+takkun2355@users.noreply.github.com>
2026-07-15 22:26:47 +0900

    Update yubisuma.py

--- Practice/fun-game/yubisuma.py
+++ Practice/fun-game/yubisuma.py
@@ -1,6 +1,9 @@
-import questionary, random
+import questionary, random, time
 
 npc_list = ["cancel", "npc1", "npc2", "npc3", "npc4", "npc5"]
+
+exit = 0
+Start = 0
 
 while True:
     npc = questionary.select(
@@ -10,6 +13,8 @@ while True:
     if (npc == "cancel") or (npc == None):
         print("キャンセルされました")
         break
+    else:
+        Start = 1
     
     while True:
         npc_str = npc.removeprefix("npc")
@@ -17,6 +22,25 @@ while True:
         #B = "random.randrange(0, (A+1)-B)を使うようにしたい"
         C = 0
         
-        npc1 = npc2 = npc3 = npc4 = npc5 = random.randrange(0, (A+1))
-        
-        
+        for i in range(int(npc_str)+1):
+            if i == 0:
+                time.sleep(0.5)
+                print(
+                    "あなたのターンです。"
+                )
+                user = int(input())
+            else:
+                time.sleep(0.5)
+                print(
+                    npc_list[i] + "のターンです。"
+                )
+                NpcT1 = npcT2 = npcT3 = npcT4 = npcT5 = random.randrange(0, (A+1))
+                NpcHand1 = random.randrange(0, (A+1))
+                NpcHand2 = random.randrange(0, (A+1))
+                NpcHand3 = random.randrange(0, (A+1))
+                NpcHand4 = random.randrange(0, (A+1))
+                NpcHand5 = random.randrange(0, (A+1))
+                time.sleep(1.5)
+                
+                print(
+                )
