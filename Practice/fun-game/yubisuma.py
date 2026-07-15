146bc1812ce4c5dbed7cd56154719b0257f29c02
takkun2355 <92196121+takkun2355@users.noreply.github.com>
2026-07-15 19:16:56 +0900

    Update yubisuma.py
    cancel以外にNoneを追加し、ctrl+Cなども対応できるように変更
    代入はできていないが、とりあえずのCommit

--- Practice/fun-game/yubisuma.py
+++ Practice/fun-game/yubisuma.py
@@ -1,4 +1,4 @@
-import questionary
+import questionary, random
 
 npc_list = ["npc1", "npc2", "npc3", "npc4", "npc5", "cancel"]
 
@@ -7,6 +7,19 @@ while True:
         "対戦NPC数を決定してください。", choices=npc_list
     ).ask()
 
-    if npc == "cancel":
+    if (npc == "cancel") or (npc == None):
         print("キャンセルされました")
         break
+    
+    while True:
+        npc_str = npc - "npc"
+        npc_int = int(npc_str)
+        A = 
+        B = 
+        C = 
+        
+        npc1 = random.randrange(A, (B+1)-C)
+        npc2 = 
+        npc3 = 
+        npc4 = 
+        npc5 = 
