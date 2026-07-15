55dcf0271fa718ad51054eb9cc741841f474e238
takkun2355 <92196121+takkun2355@users.noreply.github.com>
2026-07-15 23:11:43 +0900

    Update yubisuma.py
    インデント位置の変更
    不要な代入を削除
    代入を追加

--- Practice/fun-game/yubisuma.py
+++ Practice/fun-game/yubisuma.py
@@ -4,6 +4,12 @@ npc_list = ["cancel", "npc1", "npc2", "npc3", "npc4", "npc5"]
 
 exit = 0
 Start = 0
+user_hands = 2
+Npc1_hands = 2
+Npc2_hands = 2
+Npc3_hands = 2
+Npc4_hands = 2
+Npc5_hands = 2
 
 while True:
     npc = questionary.select(
@@ -20,7 +26,7 @@ while True:
         npc_str = npc.removeprefix("npc")
         A = ((int(npc_str) * 2) + 2)
         #B = "random.randrange(0, (A+1)-B)を使うようにしたい"
-        C = 0
+        total_hand = 0
         
         for i in range(int(npc_str)+1):
             if i == 0:
@@ -34,13 +40,16 @@ while True:
                 print(
                     npc_list[i] + "のターンです。"
                 )
-                NpcT1 = npcT2 = npcT3 = npcT4 = npcT5 = random.randrange(0, (A+1))
-                NpcHand1 = random.randrange(0, (A+1))
-                NpcHand2 = random.randrange(0, (A+1))
-                NpcHand3 = random.randrange(0, (A+1))
-                NpcHand4 = random.randrange(0, (A+1))
-                NpcHand5 = random.randrange(0, (A+1))
-                time.sleep(1.5)
                 
-                print(
-                )
+            NpcT1 = NpcT2 = NpcT3 = NpcT4 = NpcT5 = random.randrange(0, (A+1))
+            NpcHand1 = random.randrange(0, (A+1))
+            NpcHand2 = random.randrange(0, (A+1))
+            NpcHand3 = random.randrange(0, (A+1))
+            NpcHand4 = random.randrange(0, (A+1))
+            NpcHand5 = random.randrange(0, (A+1))
+            time.sleep(1.5)
+            
+            print(
+            )
+            if total_hand == user:
+                
