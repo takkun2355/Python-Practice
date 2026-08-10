021d17375acf8864f21ec4a192e8e34be481cd78
takkun2355 <92196121+takkun2355@users.noreply.github.com>
2026-08-10 23:58:52 +0900

    Update yubisuma.py

--- Practice/fun-game/yubisuma.py
+++ Practice/fun-game/yubisuma.py
@@ -2,28 +2,21 @@ import questionary, random, time
 
 npc_list = ["cancel", "npc1", "npc2", "npc3", "npc4", "npc5"]
 
-exit = 0
-Start = 0
 user_hands = 2
-Npc1_hands = 2
-Npc2_hands = 2
-Npc3_hands = 2
-Npc4_hands = 2
-Npc5_hands = 2
+Npc_hands = ["NPC1": 2,"NPC2": 2,"NPC3": 2,"NPC4": 2,"NPC5": 2]
 
 while True:
-    npc = questionary.select(
+    select = questionary.select(
         "対戦NPC数を決定してください。", choices=npc_list
     ).ask()
 
-    if (npc == "cancel") or (npc == None):
+    if (select == "cancel") or (select == None):
         print("キャンセルされました")
         break
-    else:
-        Start = 1
     
     while True:
-        npc_str = npc.removeprefix("npc")
+        npc_str = select.removeprefix("npc")
+
         A = ((int(npc_str) * 2) + 2)
         #B = "random.randrange(0, (A+1)-B)を使うようにしたい"
         total_hand = 0
@@ -32,7 +25,8 @@ while True:
             if i == 0:
                 time.sleep(0.5)
                 print(
-                    "あなたのターンです。"
+                    "あなたのターンです。",
+                    "数を指定して"
                 )
                 user = int(input())
             else:
